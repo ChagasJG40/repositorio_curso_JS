4c639039dbc59b79fa329144993bdbef792f4b98
ChagasJG40 <harrisonbchagas@gmail.com>
2023-01-25 14:18:09 -0400

    iniciando o código

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('Javascript Funcionando');
+
+let firstName = 'George';
+let lastName = 'Chagas'
+let eyeColor = 'Azuis'
+let peleCor = 'Branca'
+console.log(firstName);
+console.log(lastName);
+console.log(eyeColor);
+console.log(peleCor);
